9fc1b22ec0e66e792ff8d62e00b9d2119dbdfd99
Victor Ngo <psychosete@gmail.com>
2018-01-27 20:06:13 -0800

    (feat) (front-end) Import js file

--- README.md
+++ README.md
@@ -35,7 +35,7 @@
 
 # CORE LOGIC SETUP
 
-*/
+
 var input = [5, 3, 7, 2, 6, 4, 5, 9, 1, 2]; // Height of walls
 var output = [3, 8, 11]; // Most water trapped between 2 walls
 /*
---
+++ client/waterwalls.js
@@ -0,0 +1,15 @@
+
+window.onload = function () {
+  /*
+
+    var input = [5, 3, 7, 2, 6, 4, 5, 9, 1, 2]; // Height of walls
+    var output = [3, 8, 11]; // Most water trapped between 2 walls  
+
+    // width is 10 default
+    // height is 10 default
+
+    // 
+*/
+
+  console.log('HITTT');
+};
